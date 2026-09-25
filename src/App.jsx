267a246781985a1f677a9dad06c6b07ea87e2d91
Manav v <83264523+kishan7785@users.noreply.github.com>
2026-09-26 01:04:04 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ export default function App() {
           <ScrollManager />
           <Navbar />
           <AnimatedRoutes />
-          {/* <Footer /> */}
+          <Footer />
         </div>
       </ThemeProvider>
     </BrowserRouter>
